03b47d3fc8530fd178e2e19db1e454274dafba7c
Jeremy Wu <jrywjwu@gmail.com>
2011-09-22 08:42:29 +0800

    add ime register option.

--- HelperApps/GacUtil/Program.cs
+++ HelperApps/GacUtil/Program.cs
@@ -1,12 +1,23 @@
 using System;
 using System.EnterpriseServices.Internal;
+using System.Runtime.InteropServices;
+
+
+
 namespace GacUtil
 {
+    
+    
     public class Program
-    {        
+    {
+        [DllImport("Imm32.dll", EntryPoint = "ImmInstallIME")]
+        public static extern IntPtr ImmInstallIMEA(string image, string name);
+
         static void Main(string[] args) 
         {
+
             Publish p = new Publish();
+
             if(args[0] == "install")
             {
                 p.GacInstall(args[1]);
@@ -14,7 +25,11 @@ namespace GacUtil
             else if (args[0] == "uninstall")
             {                
                 p.GacRemove(args[1]);
-            }            
+            }
+            else if (args[0] == "registerime")
+            {
+                ImmInstallIMEA(Environment.GetFolderPath(Environment.SpecialFolder.System)+"\\OVIME.IME", "OpenVanilla 0.9b");
+            }
         }
     }
 }
